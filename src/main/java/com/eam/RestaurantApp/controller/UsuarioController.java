844f18e77e02d8ed5493e6eaff343195fc98549c
juanmontoyagi <juanmontoya015@gmail.com>
2021-07-15 10:00:49 -0500

    Buscar, listar, actualizar usuario

--- src/main/java/com/eam/RestaurantApp/controller/UsuarioController.java
+++ src/main/java/com/eam/RestaurantApp/controller/UsuarioController.java
@@ -3,12 +3,12 @@ package com.eam.RestaurantApp.controller;
 import com.eam.RestaurantApp.entity.Usuario;
 import com.eam.RestaurantApp.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -17,8 +17,29 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @GetMapping("/{idUsuario}")
+    public Usuario find(@PathVariable("idUsuario") Integer idUsuario){
+        return usuarioService.find(idUsuario);
+    }
+
+    @GetMapping
+    public List<Usuario> getAll(@PageableDefault(size = 10, page = 0) Pageable pageable){
+        return usuarioService.getAll(pageable);
+    }
+
+    @GetMapping("/findByName/{nombreUsuario}")
+    public List<Usuario> findByName(@PathVariable("nombreUsuario") String nombreUsuario,
+                                    @PageableDefault(size = 10, page = 0) Pageable pageable){
+        return usuarioService.findByName(nombreUsuario, pageable);
+    }
+
     @PostMapping
     public void create(@RequestBody @Valid Usuario usuario){
         usuarioService.create(usuario);
+    }
+
+    @PutMapping("/{idUsuario}")
+    public void update(@RequestBody @Valid Usuario usuario, @PathVariable("idUsuario") Integer idUsuario){
+        usuarioService.update(idUsuario, usuario);
     }
 }
--- src/main/java/com/eam/RestaurantApp/service/UsuarioService.java
+++ src/main/java/com/eam/RestaurantApp/service/UsuarioService.java
@@ -2,11 +2,15 @@ package com.eam.RestaurantApp.service;
 
 import com.eam.RestaurantApp.entity.Usuario;
 import com.eam.RestaurantApp.exception.BusinessException;
+import com.eam.RestaurantApp.exception.NotFoundException;
 import com.eam.RestaurantApp.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -19,6 +23,29 @@ public class UsuarioService {
         boolean usuario = usuarioRepository.existsById(user.getIdUsuario());
         if (usuario)
             throw new BusinessException("Ya existe el usuario con el ID: "+user.getIdUsuario(), "usuario_exist");
+        usuarioRepository.save(user);
+        return user;
+    }
+
+    public List<Usuario> getAll(Pageable pageable){
+        return usuarioRepository.findAll(pageable).getContent();
+    }
+
+    public List<Usuario> findByName(String nombreUsuario, Pageable pageable){
+        return usuarioRepository.findByUserName("%"+nombreUsuario+"%", pageable);
+    }
+
+    public Usuario find(Integer idUsuario){
+        boolean usuario = usuarioRepository.existsById(idUsuario);
+        if (!usuario)
+            throw new NotFoundException("No se encontró usuario con el ID "+idUsuario, "usuarios_doesnt_exist");
+        return usuarioRepository.findById(idUsuario).get();
+    }
+
+    public Usuario update(Integer idUsuario, Usuario user){
+        boolean usuario = usuarioRepository.existsById(idUsuario);
+        if (!usuario) throw new NotFoundException("No se encontró usuario con el ID "+idUsuario, "usuarios_doesnt_exist");
+        user.setIdUsuario(idUsuario);
         usuarioRepository.save(user);
         return user;
     }
