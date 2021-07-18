81e001167eeb805e618c53372e584bd251cd06b7
juanmontoyagi <juanmontoya015@gmail.com>
2021-07-17 20:44:51 -0500

    Creación componente rol

---
+++ src/main/java/com/eam/RestaurantApp/repository/RolRepository.java
@@ -0,0 +1,7 @@
+package com.eam.RestaurantApp.repository;
+
+import com.eam.RestaurantApp.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+}
